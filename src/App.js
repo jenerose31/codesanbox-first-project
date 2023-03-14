cd1af01e3db5a7f32e098b63c937db965005da34
Kervni Benitez <127859106+jenerose31@users.noreply.github.com>
2023-03-14 21:31:27 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+import React from "react";
+
+export default function App() {
+  return (
+    <>
+      <div className="App">
+        <h1>Hello CodeSandbox</h1>
+        <h2>KERVIN P. BENITEZ</h2>
+      </div>
+      <div>
+        <button onClick={() => alert("hello")}>Click me!</button>
+      </div>
+    </>
+  );
+}
